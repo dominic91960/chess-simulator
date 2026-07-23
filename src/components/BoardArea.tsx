ff1d54f4092414b1b8d68a23bba85913db2391ea
dominic91960 <dominic91960@gmail.com>
2026-07-23 22:59:14 +0530

    fix: ui improvements

--- src/components/BoardArea.tsx
+++ src/components/BoardArea.tsx
@@ -12,7 +12,7 @@ const BoardArea: React.FC<{
   return (
     <div className="bg-neutral-800 p-2 text-[24px] sm:text-[27px] md:text-[31px] lg:col-span-4 lg:h-fit lg:text-[36px] xl:text-[42px] 2xl:text-[48px]">
       <div ref={snapshotRef}>
-        <div className="relative bg-white p-[0.1em] px-[1.1em] pb-[1.1em]">
+        <div className="relative bg-white p-[0.1em] px-[1.5em] pb-[1.5em]">
           {/* Number Notations */}
           <NumberNotations />
 
@@ -21,10 +21,10 @@ const BoardArea: React.FC<{
 
           {/* Turn Indicator */}
           {turn !== "none" && (
-            <div className="absolute top-0 right-0 bottom-0 mt-[0.1em] mb-[1.1em] grid w-[1.1em] grid-cols-1 grid-rows-8 items-center justify-items-center">
+            <div className="absolute top-0 right-0 bottom-0 mt-[0.1em] mb-[1.5em] grid w-[1.5em] grid-cols-1 grid-rows-8 items-center justify-items-center">
               <div
                 className={cn(
-                  "size-[0.7em] rounded-full border-2 border-white bg-black",
+                  "size-[1em] rounded-full border-2 border-white bg-black",
                   turn === "white" && "border-black bg-white",
                 )}
               ></div>
